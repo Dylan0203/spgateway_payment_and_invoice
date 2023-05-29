aa68a209e549d0d8249e52e6f59af22e9314da34
Dylan Lin <dylanmail0203@gmail.com>
2023-05-29 12:51:08 +0800

    ♻️

--- lib/spgateway/helpers.rb
+++ lib/spgateway/helpers.rb
@@ -7,26 +7,26 @@ module Spgateway
     end
 
     def encode_post_data(data)
-      cipher = OpenSSL::Cipher::AES256.new(:CBC)
+      cipher = OpenSSL::Cipher.new('aes-256-cbc')
       cipher.encrypt
       cipher.padding = 0
-      cipher.key = @options[:hash_key]
-      cipher.iv = @options[:hash_iv]
+      cipher.key = hash_key
+      cipher.iv = hash_iv
       data = add_padding(data)
       encrypted = cipher.update(data) + cipher.final
-      encrypted.unpack('H*').first
+      encrypted.unpack1('H*')
     end
 
     def decode_aes_data(data)
-      cipher = OpenSSL::Cipher::AES256.new(:CBC)
+      cipher = OpenSSL::Cipher.new('aes-256-cbc')
       cipher.decrypt
       cipher.padding = 0
-      cipher.key = @options[:hash_key]
-      cipher.iv = @options[:hash_iv]
+      cipher.key = hash_key
+      cipher.iv = hash_iv
       data = [data].pack('H*')
       result = cipher.update(data)
 
-      return result if (result[-1] == '}')
+      return result if result[-1] == '}'
 
       result.gsub(result[-1], '')
     end
@@ -35,16 +35,16 @@ module Spgateway
       case type
       when :mpg
         check_value_fields = %i[Amt MerchantID MerchantOrderNo TimeStamp Version]
-        padded = "HashKey=#{@options[:hash_key]}&%s&HashIV=#{@options[:hash_iv]}"
+        padded = "HashKey=#{hash_key}&%s&HashIV=#{hash_iv}"
       when :query_trade_info
         check_value_fields = %i[Amt MerchantID MerchantOrderNo]
-        padded = "IV=#{@options[:hash_iv]}&%s&Key=#{@options[:hash_key]}"
+        padded = "IV=#{hash_iv}&%s&Key=#{hash_key}"
       when :credit_card_period
         check_value_fields = %i[MerchantID MerchantOrderNo PeriodAmt PeriodType TimeStamp]
-        padded = "HashKey=#{@options[:hash_key]}&%s&HashIV=#{@options[:hash_iv]}"
-      when :line_pay_refund
+        padded = "HashKey=#{hash_key}&%s&HashIV=#{hash_iv}"
+      when :line_pay_refund, :mpg20
         check_value_fields = %i[]
-        padded = "HashKey=#{@options[:hash_key]}&%s&HashIV=#{@options[:hash_iv]}"
+        padded = "HashKey=#{hash_key}&%s&HashIV=#{hash_iv}"
         padded = padded % params
 
         return Digest::SHA256.hexdigest(padded).upcase!
@@ -79,7 +79,7 @@ module Spgateway
     def make_check_code(params = {})
       raw = params.select { |key, _value| key.to_s.match(/^(Amt|MerchantID|MerchantOrderNo|TradeNo)$/) }
                   .sort_by { |k, _v| k.downcase }.map! { |k, v| "#{k}=#{v}" }.join('&')
-      padded = "HashIV=#{@options[:hash_iv]}&#{raw}&HashKey=#{@options[:hash_key]}"
+      padded = "HashIV=#{hash_iv}&#{raw}&HashKey=#{hash_key}"
       Digest::SHA256.hexdigest(padded).upcase!
     end
 
@@ -94,6 +94,14 @@ module Spgateway
       len = text.length
       pad = size - (len % size)
       text += (pad.chr * pad)
+    end
+
+    def hash_key
+      @options[:hash_key]
+    end
+
+    def hash_iv
+      @options[:hash_iv]
     end
   end
 end
